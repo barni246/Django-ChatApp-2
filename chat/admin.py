20ec7fc8369128a72f701b908639d7c451dd8ee9
barni246 <barnabas.gonda@googlemai.com>
2023-12-30 12:09:18 +0100

    model fields

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Message
 
 # Register your models here.
+
+class MessageAdmin(admin.ModelAdmin):
+  # fields = ('created_at',)
+    list_display = ('author','created_at','text','receiver')
+    search_fields = ('text',)
+
+admin.site.register(Message, MessageAdmin)
