e4ae476e7e849d292b502f229762826925d47f37
barni246 <barnabas.gonda@googlemai.com>
2023-12-30 20:00:49 +0100

    Chat Object joind to Message

--- chat/views.py
+++ chat/views.py
@@ -7,5 +7,6 @@ def index(request):
       #  print('Recieved data: ' + request.POST['textmessage']) Junus Version
         text_message = request.POST.get('textmessage', '')
         print('Recieved data: ' + text_message)
-        Message.objects.create(text = request.POST.get('textmessage', '' ), chat = None, author = request.user, receiver = request.user)
+        myChat = Chat.objects.get(id = 1)
+        Message.objects.create(text = request.POST.get('textmessage', '' ), chat = myChat, author = request.user, receiver = request.user)
     return render(request, 'chat/index.html', {'username': ' Barnabas'})
