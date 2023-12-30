8a5a92f84981ebe8697c77bd633beec73d9af715
barni246 <barnabas.gonda@googlemai.com>
2023-12-30 17:42:19 +0100

    Chat Object

--- chat/admin.py
+++ chat/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import Message
+from .models import Message, Chat
 
 # Register your models here.
 
 class MessageAdmin(admin.ModelAdmin):
-  # fields = ('created_at',)
+    fields = ('author','created_at','text','receiver','chat')
     list_display = ('author','created_at','text','receiver')
     search_fields = ('text',)
 
 admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
--- chat/views.py
+++ chat/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+      #  print('Recieved data: ' + request.POST['textmessage']) Junus Version
+        text_message = request.POST.get('textmessage', '')
+        print('Recieved data: ' + text_message)
     return render(request, 'chat/index.html', {'username': ' Barnabas'})
