d35612b63c6d391b5e6bd064320c64e47c676b7c
barni246 <barnabas.gonda@googlemai.com>
2024-01-02 07:32:05 +0100

    login wrongPassword

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Message, Chat
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
@@ -9,4 +11,16 @@ def index(request):
         print('Recieved data: ' + text_message)
         myChat = Chat.objects.get(id = 1)
         Message.objects.create(text = request.POST.get('textmessage', '' ), chat = myChat, author = request.user, receiver = request.user)
-    return render(request, 'chat/index.html', {'username': ' Barnabas'})
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request, 'chat/index.html', {'messages': chatMessages})
+
+
+def login_view(request):
+  if request.method == 'POST':
+    user = authenticate(username = request.POST.get('username'), password = request.POST.get('password'))
+    if user:
+      login(request, user)
+      return HttpResponseRedirect('/chat/')
+    else:
+      return render(request, 'auth/login.html', {'wrongPassword':True})
+  return render(request, 'auth/login.html')
